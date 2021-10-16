29483babb236c3ef2861dd88a6bc7d57be48bfcd
kmsheng <kmsh3ng@gmail.com>
2021-10-16 16:36:09 +0800

    update ex1.rb

--- ex1.rb
+++ ex1.rb
@@ -1,3 +1,5 @@
 puts "Hello World"
 
-puts "Double quotes \" need to be escaped."
+puts "Double quotes \" need to be escaped in double quoted string."
+
+puts 'Single quotes \' need to be escaped in single quoted string.'
