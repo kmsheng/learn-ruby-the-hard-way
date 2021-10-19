70dabdba3105fd9ae774d3b02a6bb5237b5c8022
kmsheng <kmsh3ng@gmail.com>
2021-10-19 23:18:50 +0800

    add ex5

---
+++ ex5.rb
@@ -0,0 +1,14 @@
+my_name = 'kmsheng'
+my_age = 34
+my_height = 177
+my_weight = 75
+my_eyes = 'black'
+my_teeth = 'white'
+my_hair = 'black'
+
+puts "Let's talk about %s" % my_name
+puts "My height is %dcm" % my_height
+puts "I am %dkg heavy." % my_weight
+puts "I got %s eyes and %s hair" % [my_eyes, my_hair]
+puts "My teeth is %s." % my_teeth
+puts "Triple my height is %d." % [my_height + my_height + my_height]
