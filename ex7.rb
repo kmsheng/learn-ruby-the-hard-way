de21862c02d752bc7bbb271e052434412a1fa679
kmsheng <kmsh3ng@gmail.com>
2021-10-23 16:29:48 +0800

    add ex7.rb

---
+++ ex7.rb
@@ -0,0 +1,16 @@
+puts "Mary had a little lamb."
+puts "Its fleece was white as %s." % 'snow'
+puts "And everywhere that Mary went."
+puts "." * 10  # what'd that do ?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+
+print end1 + end2 + end3 + end4 + end5 + end6
+
+# ending newline...
+puts
